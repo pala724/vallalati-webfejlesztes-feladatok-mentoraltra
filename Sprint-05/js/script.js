55b166ece7a71941cf042481a4f5b9a00913f513
Pala <paczilaci69@gmail.com>
2023-04-19 15:58:38 +0200

    update

---
+++ Sprint-05/js/script.js
@@ -0,0 +1,35 @@
+function calcAmount() {
+    let bgPrice = 1200;
+    //Read the input fields
+    let nameInput = document.querySelector("input[name='name-input']");
+    let emailInput = document.querySelector("input[name='email-input']");
+    let addressInput = document.querySelector("input[name='address-input']");
+    let commentInput = document.querySelector("input[name='comment-input']");   
+    let extraInput = document.querySelector("input[name='extra']");
+    let sauceInput = document.querySelector("select[name='sauce-input']");
+    let amountInput = document.querySelector("input[name='amount-input']");
+    let showAmount = document.querySelector("sapn.show-amount");
+    
+
+    let amountNumber = parseInt(amountInput.value);
+    amountNumber = isNaN(amountNumber) ? 0 : amountNumber;
+
+    let extraPrice = parseInt(extraInput.value);
+    extraPrice = isNaN(extraPrice) ? 0 : extraPrice;
+    let saucePrice = parseInt(sauceInput.value);
+    saucePrice = isNaN(saucePrice) ? 0 : saucePrice;
+
+    if (amountNumber > 10) {
+        alert("Maximum 10 hamburgert rendelhetsz egyszerre!");
+    }
+    else if (amountNumber < 1){
+        alert("Legalább 1 hamburgert rendelned kell!");
+    }
+    else {
+        //input to number and calculate the amount
+        let amount = (bgPrice+extraPrice+saucePrice) * amountNumber;
+        //change HTML with JS and show amount
+        showAmount.innerHTML = amount;
+    }
+
+}
